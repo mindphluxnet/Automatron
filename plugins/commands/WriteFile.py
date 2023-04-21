cb71f6ed313c3c55d1ccfba61d5728dae36fba92
Richard Kämmerer <richard.kaemmerer@gmail.com>
2023-04-21 15:47:22 +0200

    WriteFile command

---
+++ plugins/commands/WriteFile.py
@@ -0,0 +1,27 @@
+from plugins.BasePlugin import BasePlugin
+from dotenv import load_dotenv
+import os
+
+
+class WriteFile(BasePlugin):
+    def __init__(self, plugin_manager: object):
+        load_dotenv()
+        self.command = "write_file"
+        self.description = "Write text to a file"
+        self.arguments = {"file_name": "<file name>", "content": "<the text to write>"}
+        self.returns = str
+        self.feed_back = True
+        self.plugin_manager = plugin_manager
+
+    def register(self):
+        return self.command, self.description, self.arguments, self.returns, self.feed_back
+
+    def run_plugin(self, arguments: dict[str, any]):
+        self.plugin_manager.logger.info(f'Command {self.command} called with arguments: {arguments}')
+        if not os.path.isfile(os.path.join(os.environ["AGENT_WORKSPACE"], arguments["file_name"])):
+            with open(os.path.join(os.environ["AGENT_WORKSPACE"], arguments["file_name"]), "w") as f:
+                f.write(arguments["content"])
+                return f'The file {arguments["file_name"]} has been written successfully.'
+
+        return f'The file {arguments["file_name"]} already existed in your workspace. You might want to use ' \
+               f'the "append_file" command instead.'
