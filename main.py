4fe34573b8dcd91c1c6a75a8da002387f26817c3
Richard Kämmerer <richard.kaemmerer@gmail.com>
2023-04-23 00:39:07 +0200

    Startup message, pressing enter without entering anything no longer queries ChatGPT

--- main.py
+++ main.py
@@ -17,8 +17,10 @@ def run_command(command: str, arguments: dict[str, any]):
 
 
 if __name__ == "__main__":
-    agent = Agent()
     init()
+    print(f"{Fore.LIGHTYELLOW_EX}Automatron{Style.RESET_ALL}, an experimental ChatGPT automation - Copyright"
+          f" 2023 Richard Kämmerer")
+    agent = Agent()
     keep_going = True
     rerun_query = None  # Set if an error occurred, this is the query to try again.
     next_query = None  # Set if a command wants to feed back to ChatGPT.
@@ -46,37 +48,39 @@ if __name__ == "__main__":
         if query == "q":
             exit(0)
         else:
-            with Halo(text='AI is thinking ...', spinner='dots'):
-                # If an error occurs wait_time and wait_reason will be set. This causes the app to wait for
-                # wait_time seconds before retrying the query. This should only happen if the ChatGPT API is
-                # overloaded and returns empty responses.
-                result, wait_time, wait_reason = agent.query(query)
+            if query != "":
+                with Halo(text='AI is thinking ...', spinner='dots'):
+                    # If an error occurs wait_time and wait_reason will be set. This causes the app to wait for
+                    # wait_time seconds before retrying the query. This should only happen if the ChatGPT API is
+                    # overloaded and returns empty responses.
+                    result, wait_time, wait_reason = agent.query(query)
 
-            if result:
-                print(f'[{Fore.YELLOW}${agent.cost_manager.get_session_cost():.2f}{Style.RESET_ALL}] '
-                      f'{Fore.LIGHTGREEN_EX}Agent:{Style.RESET_ALL} {result[0]["thoughts"]["text"]}')
-                if "name" in result[0]["command"]:
-                    command_to_run = result[0]["command"]["name"]
-                    if not command_to_run == "task_complete" and not command_to_run == "do_nothing":
-                        result = run_command(command_to_run, result[0]["command"]["args"])
-
-                        if result is not None:
-                            if agent.plugin_manager.should_feed_back(command_to_run):
-                                next_query = result
-                    # The commands task_complete and do_nothing are virtual. They exist as plugins but only for sorting
-                    # purposes so they are listed last in the command list. The actual plugin is never used.
-                    elif command_to_run == "task_complete":
-                        print(f"Agent wants to run command {command_to_run} which indicates it considers the task "
-                              f"to be complete.")
-                        exit()
-                    elif command_to_run == "do_nothing":
-                        print(f"Agent wants to run command {command_to_run} which does nothing. "
-                              f"You can assume it's done with the task.")
-
-                else:
+                if result:
                     print(f'[{Fore.YELLOW}${agent.cost_manager.get_session_cost():.2f}{Style.RESET_ALL}] '
-                          f'{Fore.GREEN}Agent response:{Style.RESET_ALL} {result[0]["thoughts"]["text"]}')
-            else:
-                if wait_time > 0:
-                    wait(wait_time, wait_reason)
-                    rerun_query = query
+                          f'{Fore.LIGHTGREEN_EX}Agent:{Style.RESET_ALL} {result[0]["thoughts"]["text"]}')
+                    if "name" in result[0]["command"]:
+                        command_to_run = result[0]["command"]["name"]
+                        if not command_to_run == "task_complete" and not command_to_run == "do_nothing":
+                            result = run_command(command_to_run, result[0]["command"]["args"])
+
+                            if result is not None:
+                                if agent.plugin_manager.should_feed_back(command_to_run):
+                                    next_query = result
+                        # The commands task_complete and do_nothing are virtual. They exist as plugins but only for
+                        # sorting purposes, so they are listed last in the command list.
+                        # The actual plugin is never used.
+                        elif command_to_run == "task_complete":
+                            print(f"Agent wants to run command {command_to_run} which indicates it considers the task "
+                                  f"to be complete.")
+                            exit()
+                        elif command_to_run == "do_nothing":
+                            print(f"Agent wants to run command {command_to_run} which does nothing. "
+                                  f"You can assume it's done with the task.")
+
+                    else:
+                        print(f'[{Fore.YELLOW}${agent.cost_manager.get_session_cost():.2f}{Style.RESET_ALL}] '
+                              f'{Fore.GREEN}Agent response:{Style.RESET_ALL} {result[0]["thoughts"]["text"]}')
+                else:
+                    if wait_time > 0:
+                        wait(wait_time, wait_reason)
+                        rerun_query = query
